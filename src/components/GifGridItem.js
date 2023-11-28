7bcd9041b3dc93f921187d82250af6147e5d8969
Carlos Niño <124508804+CarlosNino93@users.noreply.github.com>
2023-11-28 17:39:20 -0400

    animation time update

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const GifGridItem = ({ id, title, url }) => {
     return (
-        <div className="card animate__animated animate__bounce">
+        <div className="card animate__animated animate__fadeIn">
             <img id={id} src={url} alt="error" style={{ width: 200 }}></img>
             <h3>{title}</h3>
         </div>
