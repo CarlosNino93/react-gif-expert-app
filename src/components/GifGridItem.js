4bdc3a3c0280a592b334698a4720606e8f4915d2
Carlos Niño <124508804+CarlosNino93@users.noreply.github.com>
2023-11-28 10:04:18 -0400

    add styles to the cards

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const GifGridItem = ({ id, title, url }) => {
     console.log(id, title, url);
     return (
-        <div>
+        <div className="card">
             <img id={id} src={url} alt="error" style={{ width: 200 }}></img>
             <h3>{title}</h3>
         </div>
