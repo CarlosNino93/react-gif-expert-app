0800135e86eedb4b77c547cf16a33cd201f81115
Carlos Niño <124508804+CarlosNino93@users.noreply.github.com>
2023-11-28 17:16:41 -0400

    deploy-done

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const GifGridItem = ({ id, title, url }) => {
     return (
-        <div className="card animate__fadeIn">
+        <div className="card animate__animated animate__bounce">
             <img id={id} src={url} alt="error" style={{ width: 200 }}></img>
             <h3>{title}</h3>
         </div>
